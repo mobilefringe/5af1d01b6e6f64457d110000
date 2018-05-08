3c3c3e50a7e83cc2098bc68b40ab306462851c5d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-08 14:47:22 -0400

    Turquoise Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -2,6 +2,9 @@
     <div> <!-- without an outer container div this component template will not render -->
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
+            <inside-header-component></inside-header-component>
+        </transition>
+        <transition name="fade">
             <div v-if="dataLoaded" v-cloak class="main_container margin_30">
                 <div class="hidden_phone">
                     <div class="row store_nav">
