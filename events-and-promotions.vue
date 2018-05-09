fbdf3a8cf314fdef1bc88626450f7075dd50bdd5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-09 17:41:13 -0400

    Turquoise Template 1.0  /events-and-promotions.vue

--- events-and-promotions.vue
+++ events-and-promotions.vue
@@ -158,6 +158,9 @@
                             if (_.includes(value.image_url, 'missing')) {
                                 value.image_url = "http://placehold.it/400x400";
                             }
+                            
+                            value.description_short = _.truncate(value.description, { 'length': 150, 'separator': ' ' });
+                            
                             showPromos.push(value);
                         }
                     });
