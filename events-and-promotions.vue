12f61e7f048ce5a84efb354cbd962afcbc5b0938
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-09 16:06:12 -0400

    Turquoise Template 1.0  /events-and-promotions.vue

---
+++ events-and-promotions.vue
@@ -0,0 +1,216 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_header_background">
+                    <div class="main_container">
+                        <div class="page_container">
+                            <h2>{{ currentStore.name }}</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="main_container margin_30">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="http://placehold.it/1200x440/757575" alt="" />    
+                        </div>
+                        <div class="col-md-9">
+                            
+                            <div v-if="this.currentStore.promotions">
+                                <b-card no-body class="mb-1 inside_page_toggle">
+                                    <b-card-header header-tag="header" class="p-1" role="tab">
+                                        <b-btn block @click="togglePromos = !togglePromos" :aria-expanded="togglePromos ? 'true' : 'false'" aria-controls="togglePromotions">
+                                            Promotions
+                                            <i v-if="togglePromos"  class="fa fa-minus f"></i>
+                                            <i v-else  class="fa fa-plus"></i>
+                                        </b-btn>
+                                    </b-card-header>
+                                    <b-collapse v-for="promo in storePromotions" v-model="togglePromos" role="tabpanel" id="togglePromotions" class="accordion_body">
+                                        <b-card-body>
+                                            <div class="row">
+                                                <div class="col-md-5" v-if="">
+                                                    <img :src="promo.image_url" :alt="promo.name" class="" />
+                                                </div>
+                                                <div class="col-md-7">
+                                                    <p class="promo_name">{{promo.name}}</p>
+                                                    <p class="promo_date" v-if="isMultiDay(promo)">
+                        							    {{ promo.start_date | moment("MMMM D", timezone)}} to {{ promo.end_date | moment("MMMM D", timezone)}}
+                                                    </p>
+                                                    <p class="promo_date" v-else>{{ promo.start_date | moment("MMMM D", timezone)}}</p>
+                                                    <div class="promo_desc" v-html="promo.description_short"></div>
+                                                    <router-link :to="'/promotions/'+ promo.slug" >
+							                            <a class="read_more">Promo Details</a>
+					                                </router-link>
+                                                </div>
+                                            </div>
+                                            <hr class="promo_separator" />
+                                        </b-card-body>
+                                    </b-collapse>
+                                </b-card>
+                            </div>
+                            <div v-if="this.currentStore.jobs">
+                                <b-card no-body class="mb-1 inside_page_toggle">
+                                    <b-card-header header-tag="header" class="p-1" role="tab">
+                                        <b-btn block @click="toggleJobs = !toggleJobs" :aria-expanded="toggleJobs ? 'true' : 'false'" aria-controls="toggleJobs">
+                                            Jobs
+                                            <i v-if="toggleJobs"  class="fa fa-minus f"></i>
+                                            <i v-else  class="fa fa-plus"></i>
+                                        </b-btn>
+                                    </b-card-header>
+                                    <b-collapse v-for="job in storeJobs" v-model="toggleJobs" role="tabpanel" id="toggleJobs" class="accordion_body">
+                                        <b-card-body>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <p class="promo_name">{{job.name}}</p>
+                                                    <p class="promo_date" v-if="isMultiDay(job)">
+                        							    {{ job.start_date | moment("MMMM D", timezone)}} to {{ job.end_date | moment("MMMM D", timezone)}}
+                                                    </p>
+                                                    <p class="promo_date" v-else>{{ job.start_date | moment("MMMM D", timezone)}}</p>
+                                                    <router-link :to="'/jobs/'+ job.slug" >
+							                            <a class="read_more">View Job Details</a>
+					                                </router-link>
+                                                </div>
+                                            </div>
+                                            <hr class="promo_separator" />
+                                        </b-card-body>
+                                    </b-collapse>
+                                </b-card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "moment", "bootstrap-vue"], function (Vue, Vuex, Meta, moment, BootstrapVue) {
+        Vue.use(BootstrapVue);
+        return Vue.component("events-and-promotions-component", {
+            template: template, // the variable template will be injected,
+            props: ['id'],
+            data: function () {
+                return {
+                    dataLoaded: false,
+                    currentStore: null,
+                    storeHours: null,
+                    storePromotions: null,
+                    togglePromos: false,
+                    storeJobs: null,
+                    toggleJobs: false,
+                    map: null
+                }
+            },
+            created (){
+                this.loadData().then(response => {
+                    this.dataLoaded = true;
+                    this.updateCurrentStore(this.id);
+                });
+            },
+            watch: {
+                $route: function () {
+                    this.updateCurrentStore(this.$route.params.id);
+                },
+                currentStore: function () {
+                    var vm = this;
+                    var storeHours = [];
+                    _.forEach(this.currentStore.store_hours, function (value, key) {
+                        storeHours.push(vm.findHourById(value));
+                    });
+                    this.storeHours = storeHours;
+                    
+                    var vm = this;
+                    var temp_promo = [];
+                    _.forEach(this.currentStore.promotions, function(value, key) {
+                        var current_promo = vm.findPromoById(value);
+                        
+                        if (_.includes(current_promo.image_url, 'missing')) {
+                            current_promo.image_url = "http://placehold.it/1560x800/757575";
+                        }
+                        current_promo.description_short = _.truncate(current_promo.description, { 'length': 150, 'separator': ' ' });
+
+                        temp_promo.push(current_promo);
+                    }); 
+                    this.storePromotions = temp_promo;
+                    this.togglePromos = true;
+                    
+                    var vm = this;
+                    var temp_job = [];
+                    _.forEach(this.currentStore.jobs, function(value, key) {
+                        var current_job = vm.findJobById(value);
+                        temp_job.push(current_job);
+                    }); 
+                    this.storeJobs = temp_job;
+                    this.toggleJobs = true;
+                },
+                // map : function (){
+                    
+                //         var vm = this;
+                //         setTimeout(function () {
+                //             vm.dropPin();
+                //         }, 500);
+                   
+                // }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'stores',
+                    'findStoreBySlug',
+                    'findCategoryById',
+                    'findHourById',
+                    'findPromoById',
+                    'findJobById'
+                ]),
+                storeCategory() {
+                    var currentStoreCategory = this.currentStore.categories[0];
+                    category = this.findCategoryById(currentStoreCategory)
+                    return category.name
+                },
+                getSVGurl () {
+                    return "https://www.mallmaverick.com" + this.property.svgmap_url;
+                },
+                svgMapRef () {
+                    return _.filter(this.$children, function(o) { return (o.$el.className == "svg-map") })[0];
+                }
+            },
+            methods: {
+                loadData: async function () {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData","jobs")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                updateCurrentStore(id) {
+                    this.currentStore = this.findStoreBySlug(id);
+                    if (this.currentStore === null || this.currentStore === undefined) {
+                        this.$router.replace({ name: 'stores' });
+                    }
+                    console.log(this.currentStore)
+                },
+                updateSVGMap (map) {
+                    this.map = map;
+                    this.dropPin();
+                },
+                dropPin () {
+                    this.svgMapRef.addMarker(this.currentStore, '//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
+                    this.svgMapRef.setViewBox(this.currentStore)
+                },
+                isMultiDay(promo) {
+                    var timezone = this.timezone
+                    var start_date = moment(promo.start_date).tz(timezone).format("MM-DD-YYYY")
+                    var end_date = moment(promo.end_date).tz(timezone).format("MM-DD-YYYY")
+                    if (start_date === end_date) {
+                        return false
+                    } else {
+                        return true
+                    }
+                }
+            }
+        });
+    });
+</script>
