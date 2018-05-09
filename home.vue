07b0d5bd991ea66b0962c18777768fc695154736
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-09 10:43:29 -0400

    Turquoise Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -19,7 +19,7 @@
                 <div class="main_container">
                     <div v-if="featureItems" class="row">
                         <div v-for="item in featureItems" class="col-md-4 feature_item">
-                            <img :src="item.image_url" alt="item.name" />
+                            <img :src="item.image_url" :alt="item.name" />
                             <!--<img src="//codecloud.cdn.speedyrails.net/sites/5af1d01b6e6f64457d110000/image/png/1518461604000/Stores Directory.png" />-->
                         </div>
                     </div>
