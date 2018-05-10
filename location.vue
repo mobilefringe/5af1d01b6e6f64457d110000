6332d86992d4b368d5e1964d9d5c646942f05a4a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-10 15:14:49 -0400

    Turquoise Template 1.0  /location.vue

--- location.vue
+++ location.vue
@@ -20,7 +20,8 @@
                                 2275 Village Walk Drive Henderson, Nevada 89052
                                 {{ property.name }} <br>
                                 {{ property.address1 }} <br>
-                                {{ property.
+                                {{ property.city }}, {{ property.province_state }} <br>
+                                {{ property.postal_code }}
                             </p>
                             <a class="main_btn block animated_btn" href="https://maps.google.com/maps?ll=36.022438,-115.08492&z=16&t=m&hl=en-US&gl=CA&mapclient=embed&daddr=2225%20Village%20Walk%20Dr%20%23171%20Henderson%2C%20NV%2089052%20USA@36.022438,-115.0849203" target="_blank" >Driving Direction</a>   
                         </div>
@@ -59,7 +60,6 @@
                 this.loadData().then(response => {
                     // this.currentPage = response[0].data;
                     this.dataLoaded = true;
-                    console.log(this.property)
                 });
             },
             computed: {
