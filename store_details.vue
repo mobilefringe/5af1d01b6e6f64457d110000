c215d979704679554b119b7665da8a2a29b835cb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-11 16:18:42 -0400

    Turquoise Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -31,13 +31,12 @@
                             <div class="inside_page_header">Store Hours & Information</div>
                             <ul v-if="storeHours" class="store_details_hours_list">
                                 <li v-for="hour in storeHours">
-                                    <span v-if="!hour.is_closed">
-                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}}</span> 
-                                        {{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}
-                                    </span>
-                                    <span v-else>
-                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}}: </span> CLOSED
-                                    </span>
+                                    <div v-if="!hour.is_closed">
+                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>{{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}
+                                    </div>
+                                    <div v-else>
+                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>CLOSED
+                                    </div>
                                 </li>
                                 
                             </ul>
