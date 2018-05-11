b6bf18dcb17766f005c9bcd9c45f6e97f089f7fa
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-10 21:40:36 -0400

    Turquoise Template 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -93,18 +93,18 @@ define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue
                     var jobs = this.processedJobs;
                     var showJobs = [];
                     _.forEach(jobs, function (value, key) {
-                        var today = moment.tz(this.timezone).format();
-                        var showOnWebDate = moment.tz(value.show_on_web_date, this.timezone).format();
-                        if (today >= showOnWebDate) {
-                            if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
-                                value.store.image_url = "http://placehold.it/400x400";
-                            }
+                        // var today = moment.tz(this.timezone).format();
+                        // var showOnWebDate = moment.tz(value.show_on_web_date, this.timezone).format();
+                        // if (today >= showOnWebDate) {
+                        //     if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
+                        //         value.store.image_url = "http://placehold.it/400x400";
+                        //     }
                             
-                            if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "http://placehold.it/400x400";
-                            }
+                        //     if (_.includes(value.image_url, 'missing')) {
+                        //         value.image_url = "http://placehold.it/400x400";
+                        //     }
                             
-                            value.description_short = _.truncate(value.description, { 'length': 100, 'separator': ' ' });
+                        //     value.description_short = _.truncate(value.description, { 'length': 100, 'separator': ' ' });
                             
                             showJobs.push(value);
                         }
