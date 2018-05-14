4077191d9854791188d09662fc2508ba800580a7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-14 15:46:38 -0400

    Turquoise Template 1.0  /store_test.vue

--- store_test.vue
+++ store_test.vue
@@ -40,7 +40,9 @@
                                             <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
                                                 {{store.name}}
                                             </router-link>
-                                            <span v-if="store.is_new_store" class="pull-right new_store">NEW</span>
+                                            <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i>
+
+</span>
                                             <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i>
 
 </span>
@@ -61,7 +63,9 @@
                                             <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
                                                 {{store.name}}
                                             </router-link>
-                                            <span v-if="store.is_new_store" class="pull-right new_store">NEW</span>
+                                            <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i>
+
+</span>
                                             <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i>
 
 </span>
@@ -84,7 +88,9 @@
                                             <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
                                                 {{store.name}}
                                             </router-link>
-                                            <span v-if="store.is_new_store" class="pull-right new_store">NEW</span>
+                                            <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i>
+
+</span>
                                             <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i>
 
 </span>
