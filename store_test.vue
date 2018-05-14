25ca42f36a8004b341cc82fd11db8825b4ca1074
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-14 17:04:26 -0400

    Turquoise Template 1.0  /store_test.vue

--- store_test.vue
+++ store_test.vue
@@ -224,10 +224,8 @@
                         this.filteredStores = sortedCats;
                     }
                 },
-                dropPin (store) {
-                    this.svgMapRef.hideMarkers();
-                    this.svgMapRef.addMarker(store, '//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
-                    this.svgMapRef.setViewBox(store)
+                dropPin(store) {
+                    this.pngMapRef.showLocation(store.id);
                 }
             }
         });
