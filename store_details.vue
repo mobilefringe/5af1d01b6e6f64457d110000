0d0fe3a18397346b808a488cea05f69420d4f1a4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-14 11:28:02 -0400

    Turquoise Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -224,7 +224,6 @@
                     if (this.currentStore === null || this.currentStore === undefined) {
                         this.$router.replace({ name: 'stores' });
                     }
-                    console.log(this.currentStore)
                 },
                 updateSVGMap (map) {
                     this.map = map;
